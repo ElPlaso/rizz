5fa412f7de4bf2426b0b5952d91fbbbf2af177a2
Moo Plah Soe Kusay <plahsoekusay@gmail.com>
2025-08-28 23:07:37 +1200

    Fetch question

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use exitfailure::ExitFailure;
+use reqwest::Url;
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TriviaQuestion {
+    response_code: f64,
+    results: [TriviaResult; 1],
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TriviaResult {
+    question: String,
+    correct_answer: String,
+}
+
+impl TriviaQuestion {
+    async fn get(
+        category: &String,
+        difficulty: &String,
+        question_type: &String,
+    ) -> Result<Self, ExitFailure> {
+        let url = format!(
+            "https://opentdb.com/api.php?amount=1&category={}&difficulty={}&type={}",
+            category, difficulty, question_type
+        );
+
+        let url = Url::parse(&*url)?;
+        let res = reqwest::get(url).await?.json::<TriviaQuestion>().await?;
+
+        Ok(res)
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), ExitFailure> {
+    let category = "22".to_string();
+    let difficulty = "medium".to_string();
+    let question_type = "boolean".to_string();
+
+    let res = TriviaQuestion::get(&category, &difficulty, &question_type).await?;
+    println!("Question: {}", res.results[0].question);
+
+    Ok(())
 }
